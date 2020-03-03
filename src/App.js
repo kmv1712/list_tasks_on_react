f75974070811379e6e4a70c791f181d4d0229cb4
KarpovM <kmv1712@gmail.com>
2020-03-03 09:44:05 +0500

    Делаю sidebar Все звдачич.1 5500

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import listSvg from './assets/img/list.svg'
 
 function App() {
-  return (
-    <h1>123</h1>
-  );
+  return <div className="todo">
+    <div className="todo__sidebar">
+      <ul className="todo__list">
+        <li>
+          <i>
+            <img src={listSvg} alt="List icon" />
+          </i>
+          <span>Все задачи</span>
+        </li>
+      </ul>
+    </div>
+    <div className="todo__tasks">
+      <button>fdgdfgfdgdf</button>
+
+    </div>
+
+  </div>
+
 }
 
 export default App;
