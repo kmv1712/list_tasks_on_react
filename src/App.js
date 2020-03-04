a7d79a9c4f5bf2e2f5e54a21c508322a88b83603
KarpovM <kmv1712@gmail.com>
2020-03-04 10:04:24 +0500

    Создал и начал делать компонент List для sidebar ч.1 13025

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import listSvg from './assets/img/list.svg'
+import List from './components/List'
 
 function App() {
-  return <div className="todo">
+  return (
+  <div className="todo">
     <div className="todo__sidebar">
-      <ul className="todo__list">
-        <li>
-          <i>
-            <img src={listSvg} alt="List icon" />
-          </i>
-          <span>Все задачи</span>
-        </li>
-      </ul>
+      <List items= {[{
+        icon: null,
+        name: "Все задачи",
+      },
+      {
+        icon: null,
+        name: "Покупки",
+      },
+      {
+        icon: null,
+        name: "Фронтенд",
+      },
+      {
+        icon: null,
+        name: "Фильмы и сериалы",
+      }]} />
     </div>
-    <div className="todo__tasks">
-      <button>fdgdfgfdgdf</button>
-
-    </div>
-
+    <div className="todo__tasks"></div>
   </div>
-
+  );
 }
 
 export default App;
