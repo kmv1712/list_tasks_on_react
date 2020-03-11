2290f3cae2b3630e71ddfcba24b0f4faa846c4e2
KarpovM <kmv1712@gmail.com>
2020-03-11 10:03:58 +0500

    Реализовал появление поля по кнопке Добавить список ч.2 12608

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import List from '../List';
+
+import './AddButtonList.scss';
+
+const AddButtonList = () => {
+  const [visiblePopup, setVisiblePopup] = useState(false);
+
+  return(
+    <div className="add-list">
+    <List 
+    onClick ={() => setVisiblePopup(!visiblePopup)}
+    items={[
+        {
+          className: 'list__add-button',
+          icon: <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+          ,
+          name: 'Добавить список'
+        }
+      ]}
+      isRemovable
+      />
+      {visiblePopup && (<div className="add-list__popup">
+        <input className="field" type="text" placeholder="Название списка"/>
+      </div>)}
+      </div>
+)};
+
+export default AddButtonList
+    
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,10 +3,9 @@ import classNames from 'classnames';
 
 import './List.scss';
 
-const List = ({ items, isRemovable }) => {
-  console.log(items, isRemovable);
+const List = ({ items, isRemovable, onClick }) => {
     return (
-      <ul className="list">
+      <ul onClick={onClick} className="list">
           {items.map((item, index) => (
             <li key={index} className={classNames(item.className, {'active': item.active})}>
                 <i>
